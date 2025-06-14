60918a7d5209de241b083d06c21ca6975148b2bd
male-andante <andreadanteprivitera@gmail.com>
2025-06-14 17:25:29 +0200

    aggiornato footer con solo copyright

--- Front-End/src/components/layout/Footer.jsx
+++ Front-End/src/components/layout/Footer.jsx
@@ -4,31 +4,8 @@ const Footer = () => {
     return (
         <footer className="bg-dark text-light py-4 mt-5">
             <Container>
-                <Row>
-                    <Col md={4}>
-                        <h5>Attoppa</h5>
-                        <p className="text-muted">
-                            La tua guida agli eventi techno
-                        </p>
-                    </Col>
-                    <Col md={4}>
-                        <h5>Link Utili</h5>
-                        <ul className="list-unstyled">
-                            <li><a href="/about" className="text-muted">Chi Siamo</a></li>
-                            <li><a href="/contact" className="text-muted">Contatti</a></li>
-                            <li><a href="/privacy" className="text-muted">Privacy Policy</a></li>
-                        </ul>
-                    </Col>
-                    <Col md={4}>
-                        <h5>Contatti</h5>
-                        <ul className="list-unstyled text-muted">
-                            <li>Email: info@attoppa.com</li>
-                            <li>Tel: +39 123 456 7890</li>
-                        </ul>
-                    </Col>
-                </Row>
-                <Row className="mt-3">
-                    <Col className="text-center text-muted">
+                <Row className="justify-content-center">
+                    <Col xs={12} className="text-center text-muted">
                         <small>&copy; {new Date().getFullYear()} Attoppa. Tutti i diritti riservati. Made by male_andante</small>
                     </Col>
                 </Row>
