c35f379ff5ad57f7ffc0e02ffa333e2c8e4b0ac1
male-andante <andreadanteprivitera@gmail.com>
2025-06-17 19:46:35 +0200

    inseriti log

--- Back-End/middlewares/authMiddleware.js
+++ Back-End/middlewares/authMiddleware.js
@@ -8,25 +8,35 @@ import userModel from "../models/userModel.js"
 
 // Middleware
 const authMiddleware = async (req, res, next) => {
-    //console.log("Sono authMiddleware!")
+    console.log("=== AUTH MIDDLEWARE START ===");
     try {
         const tokenBearer = req.headers.authorization
+        console.log('Authorization header:', tokenBearer);
+        
         if(tokenBearer !== undefined){
             const token = tokenBearer.replace('Bearer ', '')
+            console.log('Token estratto:', token.substring(0, 50) + '...');
+            
             const data = await verifyJWT(token)
-            console.log('Token decodificato:', data)
+            console.log('Token decodificato:', data);
+            
             if(data.exp) {
+                console.log('Token non scaduto, cercando utente con ID:', data.id);
                 const me = await userModel.findById(data.id)
                 if(me) {
+                    console.log('Utente trovato:', me.email);
                     req.user = me
                     next()
                 } else {
+                    console.log('Utente non trovato nel database');
                     res.status(401).json({ message: 'Utente non trovato!' })
                 }
             } else {
+                console.log('Token scaduto');
                 res.status(401).json({ message: 'Token scaduto! Riprova il login!' })
             }
         } else {
+            console.log('Nessun token fornito');
             res.status(401).json({ message: 'Token richiesto!' })
         } 
         
@@ -34,6 +44,7 @@ const authMiddleware = async (req, res, next) => {
         console.error('Errore nel middleware di autenticazione:', err)
         res.status(401).json({ message: 'Token non valido!' })
     }
+    console.log("=== AUTH MIDDLEWARE END ===");
 }
 
 
